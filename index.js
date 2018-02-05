51aed353bb106d4563e46201dc0dc08a3f7cebe4
krandles <krandles@gmail.com>
2018-02-05 14:59:38 -0500

    new commit

--- index.js
+++ index.js
@@ -24,3 +24,5 @@ function showCommits() {
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("commits").innerHTML = commitsList
 }
+
+
